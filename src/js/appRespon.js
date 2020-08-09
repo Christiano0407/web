de70542967dea74f2026693e5fc2c1dffdbe39ba
Christiano0407 <placebo0704@gmail.com>
2020-08-09 12:38:52 -0500

    hamburger

--- src/js/appRespon.js
+++ src/js/appRespon.js
@@ -7,4 +7,5 @@ hamburgerMenu.addEventListener(`click`, function() {
 });
 
 gsap.fromTo(`.hero-clipped`, {scaleX: 0}, {duration:1, scaleX:1});
-gsap.fromTo(`.logo`, { opacity: 0, x: 200}, {duration:1, delay: .5, x: 0, opacity: 1});
+gsap.fromTo(`.logo`, { opacity: 0, x: -200}, {duration:1, delay: .5, x: 0, opacity: 1});
+gsap.fromTo(`.hamburger-menu`, { opacity: 0, x: 200}, {duration:1, delay: .5, x: 0, opacity: 1});
