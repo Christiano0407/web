df12ed0092985e247b0fada7eac0f35c8d805031
Christiano0407 <placebo0704@gmail.com>
2020-08-24 21:14:50 -0500

    ja-1

---
+++ src/js/javasPract.js
@@ -0,0 +1,13 @@
+/* const hello = document.querySelector(".hello"); */
+console.log("Hola Mundo");
+/* console.error("La aplicación ha fallado"); */
+
+document.write("Soy Javascript");
+document.write("<h1>Hello Saorse</h1>")
+/* 
+const name = {
+    "One": `Javier`,
+    "Two": "Natsha"
+}
+
+console.log(name); */
