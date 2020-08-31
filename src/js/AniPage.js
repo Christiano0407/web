1e61ee73041b478da06b93a3266fc238d17afb08
Christiano0407 <placebo0704@gmail.com>
2020-08-31 08:09:45 -0500

    slider

--- src/js/AniPage.js
+++ src/js/AniPage.js
@@ -5,4 +5,7 @@ const headline = document.querySelector(".headline");
 const burger = document.getElementById("#burger");
 
 const t1 = new TimelineMax();
-t1.fromTo(hero, 1, {height: "0%"}, {height: "80%"});
+t1.fromTo(hero, 1, {height: "0%"}, {height: "80%", ease: Power2.easeInOut})
+.fromTo(hero, 1.3, {width: "100%"}, {width: "80%", ease: Power2.easeInOut})
+
+.fromTo(slider, 1.2, {x:"-100%"}, {x:"0%", ease: Power2.easeInOut})
